b9d89b0c7dd3296e4ade9cd2917f304d63fb7c0c
Saina <smacha1@gmail.com>
2024-09-24 18:29:58 -0400

    added index.html file

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+from dotenv import load_dotenv
+import os
+
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
 def home():
     return "Weather App"
 
